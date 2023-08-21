1b38c168bc4ff4c59cb3aa2a01a5541924299cab
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-08-21 11:30:51 -0300

    refactor average calculator code

--- average-calculator/Program.cs
+++ average-calculator/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+
             //Asking for the student name
             Console.WriteLine("What is the student name?");
 
@@ -19,29 +20,41 @@
             //Setting a new dynamic array with a kept length (from user input) to hold all the student grades
             float[] studentGrades = new float[gradesLength];
 
+
             //Looping through the array to ask the user to input each one of the student grades
             for (int i = 0; i < studentGrades.Length; i++)
             {
                 Console.WriteLine("Type Grade number" + (i + 1) + ":");
                 studentGrades[i] = int.Parse(Console.ReadLine());
-
             }
+
+
+            double average = CalculateAverage(studentGrades);
+
+            Console.WriteLine(average);
+
+        }
+
+
+        static double CalculateAverage(float[] args)
+        {
 
             double total = 0;
             //Looping through all grades adding each one to find the total
 
 
-            for (int i = 0; i < studentGrades.Length; i++)
+            for (int i = 0; i < args.Length; i++)
 
             {
-                total += studentGrades[i];
+                total += args[i];
             };
 
 
-            double average = total / studentGrades.Length;
+            double average = total / args.Length;
 
 
-            Console.WriteLine("Student:" + " " + studentName + " " + "Average:" + " " + average);
+            return average;
         }
+
     }
 }
